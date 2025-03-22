8c49c901d99541fd04b66cc000364e0bac8c07c4
va13k <valentyn.levytskyi@usi.ch>
2025-03-22 23:03:22 +0100

    Add Auth Controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { SignupDto } from './dto/signup.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
